3b33bfc17dea47c0e367f69b0904d08e66491040
Paul Garcia <garciaapk@gmail.com>
2017-03-12 00:35:15 -0400

    Agregando Index HomePage_revision2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "sitio/index"
-  root 'sitio#index'
+  root to:'sitio#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
